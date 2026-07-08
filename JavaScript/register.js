d51ae69fe4ea2da8fe0ae0fa2de838c7d6d06427
miyamotokaren-n <miyamoto.karen@g.wakayama-u.jp>
2026-07-08 14:26:04 +0900

    Update register.js

--- JavaScript/register.js
+++ JavaScript/register.js
@@ -1,24 +1,63 @@
-function saveClothes() {
+let imageData = "";
 
-    const category = document.getElementById("category").value;
-    const status = document.getElementById("status").value;
-    const memo = document.getElementById("memo").value;
+document.getElementById("imageInput")
+.addEventListener("change", function(e){
 
-    const clothes = {
-        category,
-        status,
-        memo
+    const file = e.target.files[0];
+
+    if(!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function(event){
+
+        imageData = event.target.result;
+
+        document.getElementById("previewImage").src =
+            imageData;
     };
 
-    let closet =
-        JSON.parse(localStorage.getItem("closet")) || [];
+    reader.readAsDataURL(file);
+});
 
+//保存
+function saveClothes() {
+
+    const category =
+        document.getElementById("category").value;
+
+    const status =
+        document.getElementById("status").value;
+
+    const memo =
+        document.getElementById("memo").value;
+
+    const clothes = {
+        image: imageData,
+        category: category,
+        status: status,
+        memo: memo
+    };
+
+    let closetData = localStorage.getItem("clothes");
+    let closet = [];
+
+    try {
+        closet = JSON.parse(closetData);
+        // 読み込んだデータが「配列」じゃなかったら、強制的に空の配列にする
+        if (!Array.isArray(closet)) {
+            closet = [];
+        }
+    } catch (e) {
+        closet = [];
+    }
+
+    // 配列に新データを追加
     closet.push(clothes);
 
-    localStorage.setItem(
-        "closet",
-        JSON.stringify(closet)
-    );
+    // 保存
+    localStorage.setItem("clothes", JSON.stringify(closet));
 
+    alert("保存しました");
     location.href = "closet.html";
 }
