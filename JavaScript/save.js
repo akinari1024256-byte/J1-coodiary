3ab170a625054ff2fa72c772721e9535b6cf93f1
Karen Miyamoto <miyamoto.karen@g.wakayama-u.jp>
2026-07-15 16:17:50 +0900

    a

--- JavaScript/save.js
+++ JavaScript/save.js
@@ -1,96 +1,44 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
+    // プレビュー画像表示
+    const image = localStorage.getItem("coordinateImage");
 
-    // プレビュー表示
-
-    const image =
-    localStorage.getItem("coordinateImage");
-
-
-    if(image){
-
-        document.getElementById("previewArea").innerHTML =
-
-        `
-        <img src="${image}" width="300">
-        `;
-
+    if (image) {
+        document.getElementById("outfitPreviewImage").src = image;
     }
 
-
-
-    // 保存ボタン
-
-    const finishSave =
-    document.getElementById("finishSave");
-
-
-    finishSave.addEventListener("click", function(){
-
-
-
-        const outfit = {
-
-
-            image:
-            localStorage.getItem("coordinateImage"),
-
-
-            date:
-            document.getElementById("date").value,
-
-
-            temp:
-            document.getElementById("temp").value,
-
-
-            weather:
-            document.getElementById("weather").value,
-
-
-            tag:
-            document.getElementById("tag").value,
-
-
-            memo:
-            document.getElementById("memo").value
-
-
-        };
-
-
-
-        // 保存済みデータ取得
-
-        let outfits =
-        JSON.parse(
-            localStorage.getItem("outfits")
-        ) || [];
-
-
-
-        // 追加
-
-        outfits.push(outfit);
-
-
-
-        // 保存
-
-        localStorage.setItem(
-            "outfits",
-            JSON.stringify(outfits)
-        );
-
-
-
-        // outfitへ移動
-
-        location.href="outfit.html";
-
-
-    });
-
-
-
 });
+
+// 保存
+function saveOutfit() {
+
+    const outfit = {
+
+        image: localStorage.getItem("coordinateImage"),
+
+        date: document.getElementById("outfitDate").value,
+
+        temp: document.getElementById("outfitTemp").value,
+
+        weather: document.getElementById("outfitWeather").value,
+
+        tag: document.getElementById("outfitTag").value,
+
+        memo: document.getElementById("outfitMemo").value
+
+    };
+
+    let outfits =
+        JSON.parse(localStorage.getItem("outfits")) || [];
+
+    outfits.push(outfit);
+
+    localStorage.setItem(
+        "outfits",
+        JSON.stringify(outfits)
+    );
+
+    alert("保存しました");
+
+    location.href = "outfit.html";
+}
